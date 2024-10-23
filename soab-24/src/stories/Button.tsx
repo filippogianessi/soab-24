f646d07ea069e1cb7ac766dc13ef8652c23ae9e2
Filippo Gianessi <gianessi.filippo@gmail.com>
2024-10-23 10:54:58 +0200

    added build

--- soab-24/src/stories/Button.tsx
+++ soab-24/src/stories/Button.tsx
@@ -5,6 +5,8 @@ import './button.css';
 export interface ButtonProps {
   /** Is this the principal call to action on the page? */
   primary?: boolean;
+  /** Which Theme? */
+  theme?: 'amazon' | 'deezer';
   /** What background color to use */
   backgroundColor?: string;
   /** How large should the button be? */
@@ -18,6 +20,7 @@ export interface ButtonProps {
 /** Primary UI component for user interaction */
 export const Button = ({
   primary = false,
+  theme = 'amazon',
   size = 'medium',
   backgroundColor,
   label,
@@ -27,6 +30,7 @@ export const Button = ({
   return (
     <button
       type="button"
+      data-theme={[`${theme}`]}
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
       {...props}
     >
